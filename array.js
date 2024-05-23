8e3e2f8c5a48e72715ccb5351d0c328c412d2790
erkansevim <erkansevim.essgmail.com>
2024-05-23 11:44:49 +0200

    *pop

---
+++ array.js
@@ -0,0 +1,25 @@
+//! Array literal
+
+const ages=[30,29,45,27,55,89];
+console.log(ages);
+
+//array constructer
+
+const cars=new Array("Mercedes-Benz", "Fiat")
+console.log(cars);
+
+const birth=1990;
+const names=["erkan", "betül", 2024-birth, cars]
+
+console.log(names);
+
+console.log(names[2]);
+
+console.log(names[3][0]);
+
+//---------- *pop-- delete last data
+
+const obst=["apfel", "kiwi", "birne"]
+obst.pop()
+
+console.log(obst);
